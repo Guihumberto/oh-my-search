bf3583dc6643321a6b7c22454a0145f316fc2759
guihumberto <juninho.joao@hotmail.com>
2024-01-18 09:20:38 -0300

    correncao ns

--- src/components/legislacao/search.vue
+++ src/components/legislacao/search.vue
@@ -41,7 +41,7 @@
                             clearable
                             chips
                             label="Fonte da norma"
-                            :items="tipos"
+                            :items="tiposA"
                             item-value="nome"
                             item-title="mudar"
                             multiple
@@ -181,7 +181,7 @@
     import docs from "@/components/legislacao/dialogs/document"
     import aggs from "@/components/legislacao/buscas/searchAggs"
 
-    import { nomeFonte } from '@/mixins/mixin'
+    import { nomeFonte } from '../../mixins/mixin'
 
     import { useGeneralStore } from '@/store/GeneralStore'
     const generalStore = useGeneralStore()  
@@ -196,6 +196,27 @@
         },
         data(){
             return{
+                tiposA:[
+                {nome:"anexos-ricms", mudar: "Anexos RICMS", sigla:"Anexos"}, 
+                {nome:"ato-declaratorio-interpretativo", mudar: "Ato Declaratório Interpretativo", sigla:"ADI"}, 
+                {nome:"atos-declaratorios", mudar: "Atos Declaratórios", sigla:"ADT"}, 
+                {nome:"beneficios", mudar: "Benefícios", sigla:"BNF"}, 
+                {nome:"beneficios-fiscais", mudar: "Benefícios Fiscais", sigla:"Ben"},
+                {nome:"consultas", mudar: "Consultas", sigla:"CST"}, 
+                {nome:"convenios", mudar: "Convênios", sigla:"CNV"}, 
+                {nome:"decretos", mudar: "Decretos", sigla:"DCT"}, 
+                {nome:"editais", mudar: "Editais", sigla:"EDT"}, 
+                {nome:"instrucoes-normativas", mudar: "Instruções Normativas", sigla:"IN"},
+                {nome:"leis-estaduais", mudar: "Leis Estaduais", sigla:"LE"}, 
+                {nome:"leis-federais", mudar: "Leis Federais", sigla:"LF"},
+                {nome:"medidas-provisorias", mudar: "Medidas Provisórias", sigla:"MP"}, 
+                {nome:"orientacao-tributaria", mudar: "Orientação Tributária", sigla:"OT"}, 
+                {nome:"portarias-conjuntas", mudar: "Portarias Conjuntas", sigla:"PC"}, 
+                {nome:"portarias", mudar: "Portarias", sigla:"PORT"}, 
+                {nome:"resolucoes", mudar: "Resoluções", sigla:"RES"},               
+                {nome:"ricms", mudar: "RICMS", sigla:"RICMS"}
+               
+            ],
                 search:{
                     text: '',
                     years: [],
