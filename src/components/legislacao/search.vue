0c42cfd8c9e335e9844dff2f02e45819bc31f162
guihumberto <juninho.joao@hotmail.com>
2024-01-18 13:21:53 -0300

    voltar saraiva

--- src/components/legislacao/search.vue
+++ src/components/legislacao/search.vue
@@ -90,7 +90,7 @@
                                 >Limpar</v-btn>
                         </div>
                         <div class="btns2 pa-2">
-                            <v-tooltip text="altera a forma de visualização">
+                            <v-tooltip text="altera para uma página ou para todas as páginas agregadas por norma">
                                 <template v-slot:activator="{ props }">
                                     <v-btn
                                         v-bind="props"
@@ -190,38 +190,6 @@
         },
         data(){
             return{
-                tiposA:[
-                    {nome:"anexos-ricms", mudar: "Anexos RICMS", sigla:"Anexos"}, 
-                    {nome:"ato-declaratorio-interpretativo", mudar: "Ato Declaratório Interpretativo", sigla:"ADI"}, 
-                    {nome:"atos-declaratorios", mudar: "Atos Declaratórios", sigla:"ADT"}, 
-                    {nome:"beneficios", mudar: "Benefícios", sigla:"BNF"}, 
-                    {nome:"beneficios-fiscais", mudar: "Benefícios Fiscais", sigla:"Ben"},
-                    {nome:"consultas", mudar: "Consultas", sigla:"CST"}, 
-                    {nome:"convenios", mudar: "Convênios", sigla:"CNV"}, 
-                    {nome:"decretos", mudar: "Decretos", sigla:"DCT"}, 
-                    {nome:"editais", mudar: "Editais", sigla:"EDT"}, 
-                    {nome:"instrucoes-normativas", mudar: "Instruções Normativas", sigla:"IN"},
-                    {nome:"leis-estaduais", mudar: "Leis Estaduais", sigla:"LE"}, 
-                    {nome:"leis-federais", mudar: "Leis Federais", sigla:"LF"},
-                    {nome:"medidas-provisorias", mudar: "Medidas Provisórias", sigla:"MP"}, 
-                    {nome:"orientacao-tributaria", mudar: "Orientação Tributária", sigla:"OT"}, 
-                    {nome:"portarias-conjuntas", mudar: "Portarias Conjuntas", sigla:"PC"}, 
-                    {nome:"portarias", mudar: "Portarias", sigla:"PORT"}, 
-                    {nome:"resolucoes", mudar: "Resoluções", sigla:"RES"},               
-                    {nome:"ricms", mudar: "RICMS", sigla:"RICMS"}
-                ],
-                periodo:[
-                    1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999,
-                    2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009,
-                    2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019,
-                    2020, 2021, 2022, 2023
-                ],
-                tipos:[
-                    "resolucoes", "portarias", "consultas", "leis-estaduais", "medidas-provisorias", "leis-federais",
-                    "editais", "portarias-conjuntas", "orientacao-tributaria", "beneficios", "instrucoes-normativas",
-                    "ato-declaratorio-interpretativo", "atos-declaratorios", "convenios", "decretos", "anexos-ricms", 
-                    "resolucoes", "ricms", "beneficios-fiscais"
-                ],
                 search:{
                     text: '',
                     years: [],
@@ -230,6 +198,18 @@
                     precision: false
                 },
                 searchOn: false,
+                periodo:[
+                    1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999,
+                    2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009,
+                    2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019,
+                    2020, 2021, 2022, 2023, 2024
+                ],
+                tipos:[
+                    "resolucoes", "portarias", "consultas", "leis-estaduais", "medidas-provisorias", "leis-federais",
+                    "editais", "portarias-conjuntas", "orientacao-tributaria", "beneficios", "instrucoes-normativas",
+                    "ato-declaratorio-interpretativo", "atos-declaratorios", "convenios", "decretos", "anexos-ricms", 
+                    "resolucoes", "ricms", "beneficios-fiscais"
+                ],
                 termos:[
                     {id:2, name: "Qualquer palavra"},
                     {id:1, name: "Frase Exata"}
