9b8d6264b5dc64efff23fc0d84dc0f4ead3d8e24
guihumberto <juninho.joao@hotmail.com>
2024-01-19 09:03:01 -0300

    periodo store

--- src/components/legislacao/porLei.vue
+++ src/components/legislacao/porLei.vue
@@ -87,6 +87,8 @@
 
 <script>
     import api from "@/services/api"
+    import { useGeneralStore } from '@/store/GeneralStore'
+    const generalStore = useGeneralStore()  
     
     export default {
         data(){
@@ -98,13 +100,6 @@
                     years: [],
                     fonte: []
                 },
-                periodo:[
-                    1984, 1985, 1986,1987, 1988, 1989,
-                    1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999,
-                    2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009,
-                    2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019,
-                    2020, 2021, 2022, 2023
-                ],
                 fontes:[
                     "resolucoes", "portarias", "consultas", "leis-estaduais", "medidas-provisorias", "leis-federais",
                     "editais", "portarias-conjuntas", "orientacao-tributaria", "beneficios", "instrucoes-normativas",
@@ -175,6 +170,9 @@
                 }, []);
 
                 return classificacao
+            },
+            periodo(){
+                return generalStore.readPeriodo
             }
         },
         methods:{
--- src/components/legislacao/search.vue
+++ src/components/legislacao/search.vue
@@ -198,12 +198,6 @@
                     precision: false
                 },
                 searchOn: false,
-                periodo:[
-                    1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999,
-                    2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009,
-                    2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019,
-                    2020, 2021, 2022, 2023, 2024
-                ],
                 tipos:[
                     "resolucoes", "portarias", "consultas", "leis-estaduais", "medidas-provisorias", "leis-federais",
                     "editais", "portarias-conjuntas", "orientacao-tributaria", "beneficios", "instrucoes-normativas",
@@ -243,6 +237,9 @@
                     this.recActive()
                 }
                 return "Home"
+            },
+            periodo(){
+                return generalStore.readPeriodo
             }
         },
         methods:{
