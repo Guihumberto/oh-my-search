67ba7f431e190bbe6bd6a296eb7ddc3d27e06596
guihumberto <juninho.joao@hotmail.com>
2024-02-05 12:32:58 -0300

    update laws

---
+++ src/components/legislacao/elements/updateLaws.vue
@@ -0,0 +1,76 @@
+<template>
+     <section>
+        <div class="container">
+            <v-card
+                class="mx-auto"
+            >
+                <v-toolbar color="cyan-lighten-1">
+
+                    <v-toolbar-title>Atualizações recente na legislação</v-toolbar-title>
+
+                    <v-spacer></v-spacer>
+
+                    <v-btn variant="text" icon="mdi-magnify"></v-btn>
+                </v-toolbar>
+
+                <v-list
+                    :items="items"
+                    item-props
+                    lines="four"
+                >
+                <template v-slot:prepend>
+                        <v-icon color="grey">mdi-text-box</v-icon>
+        
+                </template>
+                    <template v-slot:subtitle="{ subtitle }">
+                        <div v-html="subtitle"></div>
+                    </template>
+                </v-list>
+            </v-card>
+        </div>
+    </section>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      items: [
+        { type: 'subheader', title: 'esta semana' },
+        {
+          prependAvatar: 'mdi-alpha-a-box',
+          title: 'lorem ypsulom?',
+          subtitle: `<span class="text-primary">Lei 10/2030</span> &mdash; Lorem ipsum dolor sit amet. Eos obcaecati molestias eum sunt nostrum sed quia veritatis est internos fugiat hic inventore quia non omnis necessitatibus aut quisquam quasi. Aut officiis porro quo autem dolor qui quibusdam inventore qui nesciunt fugit. Ut nostrum error et corporis asperiores est voluptatum esse ex impedit alias et magnam porro 33 quos omnis.`,
+        },
+        { type: 'divider', inset: true },
+        {
+          prependAvatar: 'mdi-alpha-a-box',
+          title: 'lorem ypsulom',
+          subtitle: `<span class="text-primary">Portaria 1010/2024</span> &mdash; Lorem ipsum dolor sit amet. Eos obcaecati molestias eum sunt nostrum sed quia veritatis est internos fugiat hic inventore quia non omnis necessitatibus aut quisquam quasi. Aut officiis porro quo autem dolor qui quibusdam inventore qui nesciunt fugit. Ut nostrum error et corporis asperiores est voluptatum esse ex impedit alias et magnam porro 33 quos omnis..`,
+        },
+        { type: 'divider', inset: true },
+        {
+          prependAvatar: 'mdi-alpha-a-box',
+          title: 'lorem ypsulom',
+          subtitle: '<span class="text-primary">Medida Provisória</span> &mdash; Lorem ipsum dolor sit amet. Eos obcaecati molestias eum sunt nostrum sed quia veritatis est internos fugiat hic inventore quia non omnis necessitatibus aut quisquam quasi. Aut officiis porro quo autem dolor qui quibusdam inventore qui nesciunt fugit. Ut nostrum error et corporis asperiores est voluptatum esse ex impedit alias et magnam porro 33 quos omnis.',
+        },
+        { type: 'divider', inset: true },
+        {
+          prependAvatar: 'mdi-alpha-a-box',
+          title: 'lorem ypsulom',
+          subtitle: '<span class="text-primary">Portaria</span> &mdash; Lorem ipsum dolor sit amet. Eos obcaecati molestias eum sunt nostrum sed quia veritatis est internos fugiat hic inventore quia non omnis necessitatibus aut quisquam quasi. Aut officiis porro quo autem dolor qui quibusdam inventore qui nesciunt fugit. Ut nostrum error et corporis asperiores est voluptatum esse ex impedit alias et magnam porro 33 quos omnis.',
+        },
+        { type: 'divider', inset: true },
+        {
+          prependAvatar: 'mdi-alpha-a-box',
+          title: 'lorem ypsulom',
+          subtitle: 'Jan 20, 2014',
+          subtitle: '<span class="text-primary">REsolução</span> &mdash; Lorem ipsum dolor sit amet. Eos obcaecati molestias eum sunt nostrum sed quia veritatis est internos fugiat hic inventore quia non omnis necessitatibus aut quisquam quasi. Aut officiis porro quo autem dolor qui quibusdam inventore qui nesciunt fugit. Ut nostrum error et corporis asperiores est voluptatum esse ex impedit alias et magnam porro 33 quos omnis.',
+        },
+      ],
+    }),
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/LegesPorLei.vue
+++ src/views/LegesPorLei.vue
@@ -1,9 +1,12 @@
 <template>
     <forLaw />
+    <updateLaws />
 </template>
 
 <script setup>
   import forLaw from "@/components/legislacao/porLei.vue"
+  import updateLaws from "@/components/legislacao/elements/updateLaws"
+
 </script>
 
 <style lang="scss" scoped>
